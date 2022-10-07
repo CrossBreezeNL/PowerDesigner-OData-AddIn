3d93fd463a081291d29ec4996fe8cfbd53c63639
Harmen Wessels <97173058+harmen-xb@users.noreply.github.com>
2022-10-07 20:12:15 +0200

    PowerDesigner Add-In skeleton (#1)
    * Initial creation of empty AddIn
    * Added implementation of AddIn methods and created first OData metadata reader setup.
    * Setup basic workflow for building the add-in solution.
    * Enabled restore during build.
    * Migrated package references to proj file.
    * Updated build to use msbuild.
    * Set signing key stuff in comments.
    * Created Setup project to build msi.
    * Changed registry settings for setup project and created new msi.
    * Added PowerDesigner Type libraries for x64 build.
    * Added publishing of msi to workflow.
    * Added DisableOutOfProcBuild step in workflow.
    * Refactored workflows to have seperate workflow for feature and main and have shared action for build-assembly.
    * Added badges to README.
    * Updated installation instructions to reflact new situation with workflow artifact.
    * Added post build event for importing registry in Debug configuration (and updated README accordingly).
    * Changed product name to Add-In where AddIn was used.

---
+++ PowerDesigner_OData_AddIn/PdODataAddIn.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using PdAddInTypLib;
+
+namespace CrossBreeze.Tools.PowerDesigner.AddIn.OData
+{
+    [ComVisible(true)]
+    [Guid("87284DA7-93AC-4D80-9F83-0D5F46539B74")]
+    public class PdODataAddIn : IPdAddIn
+    {
+        // A reference to the PowerDesigner Application object.
+        private PdCommon.Application _app;
+
+        // A reference to the PowerDesigner logger object.
+        private PdLogger _logger;
+
+        const string UPDATE_MODEL_FROM_ODATA_METADATA_COMMAND = "UpdateModelFromOdataMetadata";
+        const string UPDATE_MODEL_FROM_ODATA_METADATA_CAPTION = "Update model from OData metadata";
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PdODataAddIn()
+        {
+            this._app = null;
+        }
+
+        /// <summary>
+        /// From: https://help.sap.com/docs/SAP_POWERDESIGNER/31c48596e34446a68956e0aa7e700a2e/c7d62f7d6e1b1014b6b5cd497c583efb.html
+        /// The Initialize() method initializes communication between PowerDesigner and the add-in.
+        /// PowerDesigner provides a pointer to its application object, defined in the PdCommon type library,
+        /// which allows you to access the PowerDesigner environment (output window, active model etc.).
+        /// </summary>
+        /// <param name="pApplication"></param>
+        public void Initialize(object pApplication)
+        {
+            try
+            {
+                this._app = (PdCommon.Application)pApplication;
+                this._logger = new PdLogger(this._app);
+                _logger.Info("Succesfully initialized OData AddIn");
+            }
+            catch (Exception e)
+            {
+                // Write exception in the PowerDesigner output window.
+                _logger.Error(String.Format("An exception occurred during intitialize of OData AddIn: {0}", e.Message));
+            }
+        }
+
+        /// <summary>
+        /// From: https://help.sap.com/docs/SAP_POWERDESIGNER/31c48596e34446a68956e0aa7e700a2e/c7d62f7d6e1b1014b6b5cd497c583efb.html
+        /// This functions is invoked each time PowerDesigner needs to display a menu, and returns an XML text that describes the menu items to display.
+        /// It is called once without an object parameter at the initialization of PowerDesigner to fill the Import and Reverse menus.
+        /// When you right-click a symbol in a diagram, this method is called twice: once for the object and once for the symbol.
+        /// Thus, you can create a method that is only called on graphical contextual menus.
+        /// </summary>
+        /// <param name="sMenu">This can be BrowserNode or Object</param>
+        /// <param name="pObject"></param>
+        /// <returns></returns>
+        public string ProvideMenuItems(string sMenu, object pObject)
+        {
+            _logger.Debug(String.Format("ProvideMenuItems [sMenu='{0}'; pObject={1}]", sMenu, _logger.PDObjectToString(pObject)));
+
+            // Get the class kind of the current object.
+            int pObjectClassKind = ((PdCommon.BaseObject)pObject).ClassKind;
+
+            // If the current object is a PDM model, return the menu option.
+            if (pObjectClassKind == (int)PdPDM.PdPDM_Classes.cls_Model)
+            {
+                return String.Format("<Menu><Command Name=\"{0}\" Caption=\"{1}\" /></Menu>", UPDATE_MODEL_FROM_ODATA_METADATA_COMMAND, UPDATE_MODEL_FROM_ODATA_METADATA_CAPTION);
+            }
+
+            // If nothing has return yet, return an empty string.
+            return "";
+        }
+
+        /// <summary>
+        /// From: https://help.sap.com/docs/SAP_POWERDESIGNER/31c48596e34446a68956e0aa7e700a2e/c7d62f7d6e1b1014b6b5cd497c583efb.html
+        /// This function allows you to dynamically disable commands defined in a menu. The method must return true to enable a command and false to disable it.
+        /// </summary>
+        /// <param name="sMenu"></param>
+        /// <param name="pObject"></param>
+        /// <param name="sCommandName"></param>
+        /// <returns></returns>
+        public int IsCommandSupported(string sMenu, object pObject, string sCommandName)
+        {
+            _logger.Debug(String.Format("IsCommandSupported [sMenu='{0}'; pObject={1}; sCommandName='{2}']", sMenu, _logger.PDObjectToString(pObject), sCommandName));
+
+            // Get the class kind of the current object.
+            int pObjectClassKind = ((PdCommon.BaseObject)pObject).ClassKind;
+
+            // If the current object is a PDM model, return the menu option.
+            if (pObjectClassKind == (int)PdPDM.PdPDM_Classes.cls_Model && sCommandName.Equals(UPDATE_MODEL_FROM_ODATA_METADATA_COMMAND))
+            {
+                return 1;
+            }
+
+            // If nothing has return yet, return an false.
+            return 0;
+        }
+
+        /// <summary>
+        /// From: https://help.sap.com/docs/SAP_POWERDESIGNER/31c48596e34446a68956e0aa7e700a2e/c7d62f7d6e1b1014b6b5cd497c583efb.html
+        /// This function implements the execution of a command designated by its name.
+        /// </summary>
+        /// <param name="sMenu"></param>
+        /// <param name="pObject"></param>
+        /// <param name="sCommandName"></param>
+        public void DoCommand(string sMenu, object pObject, string sCommandName)
+        {
+            // Wrap the whole DoCommand in a generic Try-Catch since exceptions are not handled by the Com Add-In automatically. The Add-In will stop working at that point.
+            try
+            {
+
+                _logger.Debug(String.Format("DoCommand [sMenu='{0}'; pObject={1}; sCommandName='{2}']", sMenu, _logger.PDObjectToString(pObject), sCommandName));
+
+                // Get the class kind of the current object.
+                int pObjectClassKind = ((PdCommon.BaseObject)pObject).ClassKind;
+
+                // If the current object is a PDM model, return the menu option.
+                if (pObjectClassKind == (int)PdPDM.PdPDM_Classes.cls_Model && sCommandName.Equals(UPDATE_MODEL_FROM_ODATA_METADATA_COMMAND))
+                {
+                    // Update the model here...
+                    PdPDM.Model pdmModel = (PdPDM.Model)pObject;
+                    new PdODataModelUpdater(this._logger).UpdatePdmModel(pdmModel);
+                }
+
+            }
+            catch (Exception e)
+            {
+                // When an Exception is thrown in C# via ActiveX it is not handled, so this way we log an error in the PowerDesigner Script output.
+                _logger.Error(string.Format("Exception was thrown during DoCommand [sMenu='{0}'; pObject={1}; sCommandName='{2}']: {3}", sMenu, _logger.PDObjectToString(pObject), sCommandName, e.Message));
+            }
+        }
+
+        /// <summary>
+        /// From: https://help.sap.com/docs/SAP_POWERDESIGNER/31c48596e34446a68956e0aa7e700a2e/c7d62f7d6e1b1014b6b5cd497c583efb.html
+        /// The Uninitialize() method is called when PowerDesigner is closed to release all global variables and clean all references to PowerDesigner objects.
+        /// </summary>
+        public void Uninitialize()
+        {
+            _logger.Debug("Uninitialize []");
+            // Unset the reference to the PowerDesigner Application object.
+            // Not sure if this is needed, but just to be sure it can't be used anymore.
+            this._app = null;
+            this._logger = null;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
+[![GitHub license](https://img.shields.io/github/license/CrossBreezeNL/PowerDesigner-OData-AddIn)](https://github.com/CrossBreezeNL/PowerDesigner-OData-AddIn/blob/main/LICENSE)
+[![GitHub issues](https://img.shields.io/github/issues/CrossBreezeNL/PowerDesigner-OData-AddIn)](https://github.com/CrossBreezeNL/PowerDesigner-OData-AddIn/issues)
+[![main build status](https://github.com/CrossBreezeNL/PowerDesigner-OData-AddIn/actions/workflows/main.yml/badge.svg)](https://github.com/CrossBreezeNL/PowerDesigner-OData-AddIn/actions/workflows/main.yml)
+[![feature build status](https://github.com/CrossBreezeNL/PowerDesigner-OData-AddIn/actions/workflows/feature.yml/badge.svg)](https://github.com/CrossBreezeNL/PowerDesigner-OData-AddIn/actions/workflows/feature.yml)
+[![GitHub commits](https://img.shields.io/github/commit-activity/m/CrossBreezeNL/PowerDesigner-OData-AddIn)](https://github.com/CrossBreezeNL/PowerDesigner-OData-AddIn/graphs/commit-activity)
+
 # PowerDesigner-OData-AddIn
 An AddIn for PowerDesigner to synchronize OData metadata with a PDM model
+
+# Installation
+In order to use the plugin, execute the MSI installer which can be downloaded as a build artifact on the latest stable [main build](https://github.com/CrossBreezeNL/PowerDesigner-OData-AddIn/actions/workflows/main.yml).
+
+Make sure to restart PowerDesigner after the installation. Then open Tools -> General Options -> Add-Ins and enable the 'OData' Add-In (by ticking the tick box before it).
+
+# Debugging
+To debug the plugin:
+
+- Open the [solution](./PowerDesigner_OData_AddIn.sln) in Visual Studio which is running as Administator (so it can register new DLLs)
+- Build the project in Debug (this will also register the Add-In in the Windows Registry for PowerDesigner).
+
+Now you can enable the Add-In in PowerDesigner in Tools -> General Options -> Add-Ins. The name of the Add-In is 'OData'.
+
+# Resources
+
+## PowerDesigner
+- https://help.sap.com/docs/SAP_POWERDESIGNER/31c48596e34446a68956e0aa7e700a2e/c7d62f7d6e1b1014b6b5cd497c583efb.html?locale=en-US&version=16.6.10
+- https://help.sap.com/docs/SAP_POWERDESIGNER/abd3434b4987485c92057ab9392aadbe/c7e194046e1b101492b38124129e7841.html?locale=en-US&version=16.6.10
+- https://help.sap.com/docs/SAP_POWERDESIGNER/31c48596e34446a68956e0aa7e700a2e/c7d0294d6e1b1014b766cb40bbc4f211.html?version=16.7.02&locale=en-US
+
+## OData
+- OData Examples: https://pragmatiqa.com/xodata/odatadir.html
+- https://learn.microsoft.com/nl-nl/odata/
